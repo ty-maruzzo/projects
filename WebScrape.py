8397db6732fa205e99097dcebe7a445c52c4d6c1
Tyrone Maruzzo <macbook@tyrones-macbook-pro.local>
2026-04-27 18:19:31 +0800

    New Tag search Ency

--- WebScrape.py
+++ WebScrape.py
@@ -2,22 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 
 
-search =["Zeus", "Hera", "Poseidon", "Demeter", "Athena", "Apollo",
-    "Artemis", "Ares", "Hephaestus", "Aphrodite", "Hermes",
-    "Hestia", "Dionysus", "Hades"]
+search =["Zeus", "Hera", "Poseidon", "Demeter", "Athena", "Apollon",
+    "Artemis", "Ares", "Hephaistos", "Aphrodite", "Hermes",
+     "Dionysos"]
 data = []
 headers = {"user-agent": "Chrome 147 on macOS (Monterey)"}
 
 
 
 for i in search:
-    url = 'https://en.wikipedia.org/wiki/' + str(i)
-    with requests.get(url,timeout=5,headers = headers) as f:
+    url = 'https://www.theoi.com/Olympios/' + str(i) + ".html"
+    with requests.get(url,headers = headers) as f:
         soup = BeautifulSoup(f.text,"lxml") 
         print(url)
         print(f.status_code)
-        paras = [a.text for a in soup.find_all('p')]
+        paras = [a.text.strip() for a in soup.find_all('p')]
         data.append(paras)
-
-
-
+        ency_tag = soup.find(id='Encyclopedia')
+        if ency_tag:
+            ency = ency_tag.find_next('p')
+        else:
+            print("No Encyclopedia id found")
+print(ency.strip())
+       
