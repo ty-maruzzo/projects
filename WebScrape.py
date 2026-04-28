3cd07128b70d2796aa0337a7195bb4f2f7fda662
Tyrone Maruzzo <macbook@tyrones-macbook-pro.local>
2026-04-28 16:54:44 +0800

    Added Myths Tag

--- WebScrape.py
+++ WebScrape.py
@@ -9,7 +9,6 @@ data = []
 headers = {"user-agent": "Chrome 147 on macOS (Monterey)"}
 
 
-
 for i in search:
     url = 'https://www.theoi.com/Olympios/' + str(i) + ".html"
     with requests.get(url,headers = headers) as f:
@@ -23,5 +22,20 @@ for i in search:
             ency = ency_tag.find_next('p')
         else:
             print("No Encyclopedia id found")
-print(ency.strip())
-       
+        myth_tag_athena = soup.find('h3', string="MYTHS")
+        if myth_tag_athena:
+            myth = myth_tag_athena.find_next('p')
+            myth2 = myth.find_next('p')
+            print(myth2.text)
+        myth_tag = soup.find('h2', string ="MYTHS")
+        myth_tag_hermes = soup.find('h2', string = 'HERMES MYTHS')
+        if myth_tag_hermes:
+            myth = myth_tag_hermes.find_next('p')
+            print(myth.text)
+        if myth_tag:
+            myth = myth_tag.find_next('p')
+            print(myth.text)
+            if myth and "famous myths" in myth.text:
+                myth = myth.find_next('p')
+                print(myth.text if myth else "No myth content found")
+        
